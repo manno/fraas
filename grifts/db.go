1a92d78909f2d714253a0863e82d4c9c7485a020
Mario Manno <mario.manno@gmail.com>
2018-05-20 10:40:06 +0200

    Add db seed task to create admin account

--- grifts/db.go
+++ grifts/db.go
@@ -1,15 +1,36 @@
 package grifts
 
 import (
+	"fmt"
+
+	"github.com/gobuffalo/pop"
 	"github.com/markbates/grift/grift"
+	"github.com/pkg/errors"
+	fh "manno.name/mm/faas-helpers"
+	"manno.name/mm/faas/models"
 )
 
 var _ = grift.Namespace("db", func() {
-
 	grift.Desc("seed", "Seeds a database")
 	grift.Add("seed", func(c *grift.Context) error {
-		// Add DB seeding stuff here
-		return nil
+		return models.DB.Transaction(func(tx *pop.Connection) error {
+			if err := tx.TruncateAll(); err != nil {
+				return errors.WithStack(err)
+			}
+
+			password := fh.GeneratePassword(12)
+			u := &models.User{
+				Email:                "admin@example.org",
+				Password:             password,
+				PasswordConfirmation: password,
+			}
+
+			if _, err := u.Create(tx); err != nil {
+				return errors.WithStack(err)
+			}
+			fmt.Println("created user admin@example.org with password: " + password)
+			return nil
+		})
 	})
 
 })
